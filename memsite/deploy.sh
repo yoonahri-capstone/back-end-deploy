995cf81fd7a78f8985ccf65b33f5964b47d94a00
ahyen <ahyen@naver.com>
2020-05-26 00:03:48 +0900

    Update deploy.sh

--- memsite/deploy.sh
+++ memsite/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /home/ubuntu/app
-pyenv global ec2_deploy
-pip3 install -r requirements.txt
-pip3 install --upgrade pip
+# pyenv global ec2_deploy
+# pip3 install -r requirements.txt
+# pip3 install --upgrade pip
 python3 manage.py runserver 0:8080
