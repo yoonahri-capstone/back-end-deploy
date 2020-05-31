56fd3d2bc2a348f9a3bf27857d56082703a01450
ahyen <ahyen@naver.com>
2020-05-31 14:34:18 +0900

    Update crawling.py

--- memsite/memmem_app/crawling.py
+++ memsite/memmem_app/crawling.py
@@ -138,7 +138,7 @@ def crawl_request(request):
                                                         'app_banner': 2, 'site_engagement': 2, 'durable_storage': 2}}
     chrome_options.add_experimental_option('prefs', prefs)
     # driver_path = os.path.abspath('memmem_app/chromedriver.exe')
-    driver_path = os.path.abspath('/home/ubuntu/app/memmem_app/memmem_app/chromedriver.exe')
+    driver_path = os.path.abspath('/home/ubuntu/app/memmem_app/chromedriver.exe')
 
     driver_path = driver_path.replace('\\', '/')
     driver = webdriver.Chrome(driver_path, options=chrome_options)
