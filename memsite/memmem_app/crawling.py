37a4a4abbb1ba66699e1a7c3b894c9338c3ac651
ahyen <ahyen@naver.com>
2020-05-31 14:20:45 +0900

    change chrome driver path

--- memsite/memmem_app/crawling.py
+++ memsite/memmem_app/crawling.py
@@ -137,7 +137,9 @@ def crawl_request(request):
                                                         'metro_switch_to_desktop': 2, 'protected_media_identifier': 2,
                                                         'app_banner': 2, 'site_engagement': 2, 'durable_storage': 2}}
     chrome_options.add_experimental_option('prefs', prefs)
-    driver_path = os.path.abspath('memmem_app/chromedriver.exe')
+    # driver_path = os.path.abspath('memmem_app/chromedriver.exe')
+    driver_path = os.path.abspath('/home/ubuntu/app/memmem_app/memmem_app/chromedriver.exe')
+
     driver_path = driver_path.replace('\\', '/')
     driver = webdriver.Chrome(driver_path, options=chrome_options)
     driver.implicitly_wait(5)
