f02e03f49982cd4afb3eed503f68f4fa32af3387
ahyen <ahyen@naver.com>
2020-06-16 16:17:31 +0900

    Update crawling.py

--- memsite/memmem_app/crawling.py
+++ memsite/memmem_app/crawling.py
@@ -12,17 +12,13 @@ from selenium.webdriver.support import expected_conditions as EC
 # URL = "https://m.blog.naver.com/binhs9576/221788249228"
 # URL = "https://www.youtube.com/watch?v=2dNc8ROMVlw"
 
-global driver, URL, html, soup, i
-i=0
+global driver, URL, html, soup
 
 def url_crawl(soup):
-    global i
     save_list = []
     CURRENT_URL = driver.current_url
     extracted = tldextract.extract(URL)
-    # print(extracted)
     extracted_current_url = tldextract.extract(CURRENT_URL)
-    # print(extracted_current_url)
     domain = extracted.domain
 
     if domain == 'instagram':
@@ -44,10 +40,10 @@ def url_crawl(soup):
         # element = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button')
         # driver.execute_script("arguments[0].click();", element)
 
-        # driver.get(CURRENT_URL)
-        # html = driver.page_source
-
-        # soup = BeautifulSoup(html, 'lxml')
+        driver.get(CURRENT_URL)
+        time.sleep(1)
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'lxml')
         
         try:
             if "비공개 계정입니다" in soup.find('h2', {"class": "rkEop"}):
@@ -187,12 +183,7 @@ def crawl_request(request):
     #                                                     'metro_switch_to_desktop': 2, 'protected_media_identifier': 2,
     #                                                     'app_banner': 2, 'site_engagement': 2, 'durable_storage': 2}}
     # chrome_options.add_experimental_option('prefs', prefs)
-    # driver_path = os.path.abspath('memmem_app/chromedriver_windossw.exe')
-    # driver_path = '/home/ubuntu/chromedriver.exe'
 
-    # driver_path = driver_path.replace('\\', '/')
-    # driver = webdriver.Chrome(executable_path = driver_path, options=chrome_options)
-    chrome_options.add_argument("user-data-dir=Selenium")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
     driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(20)
@@ -204,8 +195,8 @@ def crawl_request(request):
     if "instagram" in URL:
         # driver = webdriver.Chrome('./chromedriver_win32/chromedriver', options=chrome_options)
         driver.get("https://www.instagram.com/accounts/login/?hl=en")
-        id = 'gold_ahhyeon'
-        password = 'rmadkgus98@'
+        id = ''
+        password = ''
         id_input = driver.find_element_by_css_selector(
             '#react-root > section > main > div > article > div > div:nth-child(1) > div > form > div:nth-child(2) > div > label > input')
         id_input.send_keys(id)
@@ -214,16 +205,9 @@ def crawl_request(request):
         password_input.send_keys(password)
         password_input.submit()
         time.sleep(2)
-        driver.get(URL)
-        print(driver.page_source)
-        try:
-            WebDriverWait(driver, 10).until(
-                EC.invisibility_of_element((By.CLASS_NAME, "Kj7h1  yJx9G"))
-            )
-        except Exception as e:
-            print("에러가 이거다 이것들아", e)
-    else:
-        driver.get(URL)
+    
+
+    driver.get(URL)
 
     try:
         html = driver.page_source
